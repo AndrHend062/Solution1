4170fad1154c4ecc11e4c117f3aa1be8486f0af2
AndrHend062 <andrhend062@ed.amdsb.ca>
2017-11-28 12:51:33 -0500

    draw star not done

---
+++ drawStar/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace drawStar
+{
+    partial class drawStar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(454, 12);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 0;
+            this.drawButton.Text = "button1";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(502, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(502, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(27, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(502, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(27, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // drawStar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 305);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.drawButton);
+            this.Name = "drawStar";
+            this.Text = "Star form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ drawStar/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drawStar
+{
+    public partial class drawStar : Form
+    {
+        public drawStar()
+        {
+            InitializeComponent();
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void DrawStar(Pen pen,int xPos,int yPos,int Size )
+        {
+            Graphics g = this.CreateGraphics();
+            int x1, x2, x3, x4, x5, x6, x7, x8, x9, x10;
+            int y1, y2, y3, y4, y5, y6, y7, y8, y9, y10;
+            int scale = 133;
+
+            x1 = 50/scale;
+            y1 = 50/scale;
+            x2 = 1/scale;
+            y2 = 50 / scale;
+            x3 = 41 / scale;
+            y3 = 80 / scale;
+            x4 = 26 / scale;
+            x4 = 127 / scale;
+            y5 = 65 / scale;
+            x5 = 100 / scale;
+
+            x6 = 50 / scale;
+            y6 = 50 / scale;
+            x7 = 1 / scale;
+            y7 = 50 / scale;
+            x8 = 41 / scale;
+            y8 = 80 / scale;
+            x9 = 1 / scale;
+            x9 = 1 / scale;
+            y10 = 50 / scale;
+            x10 = 1 / scale;
+
+
+            g.DrawLine(pen, x1, y1, x2, y2);
+       
+
+
+
+        }
+
+    }
+}
